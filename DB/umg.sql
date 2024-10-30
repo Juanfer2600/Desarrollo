b27098ec94c29614a2c409245c3e6824fbb6b48b
Fernelixia <jdeleong40@miumg.edu.gt>
2024-10-29 20:18:21 -0600

    Create umg.sql

---
+++ DB/umg.sql
@@ -0,0 +1,248 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: localhost:3306
+-- Tiempo de generación: 30-10-2024 a las 02:17:57
+-- Versión del servidor: 8.0.30
+-- Versión de PHP: 8.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `umg`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `empleados`
+--
+
+DROP TABLE IF EXISTS `empleados`;
+CREATE TABLE `empleados` (
+  `id` int NOT NULL,
+  `fecha` date NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `tareas` varchar(50) NOT NULL,
+  `proyectos` varchar(50) NOT NULL,
+  `materiales` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Volcado de datos para la tabla `empleados`
+--
+
+INSERT INTO `empleados` (`id`, `fecha`, `nombre`, `tareas`, `proyectos`, `materiales`) VALUES
+(1, '2024-10-05', 'Juan Fernando', '2', '1', 'N/A'),
+(2, '2024-10-05', 'Pablo Delgado', '2', '1', '1'),
+(3, '2024-10-19', 'Pablo Rodrigo', '2', 'N/A', '1');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `materiales`
+--
+
+DROP TABLE IF EXISTS `materiales`;
+CREATE TABLE `materiales` (
+  `id` int NOT NULL,
+  `codigo` varchar(50) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `direccion` mediumtext NOT NULL,
+  `tipo` varchar(50) NOT NULL,
+  `cantidad` varchar(50) NOT NULL,
+  `estado` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Volcado de datos para la tabla `materiales`
+--
+
+INSERT INTO `materiales` (`id`, `codigo`, `nombre`, `direccion`, `tipo`, `cantidad`, `estado`) VALUES
+(1, '1234', 'Pinzas', 'Colonia Hunapu', 'Herramienta', '5', 'Disponible');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `proyectos`
+--
+
+DROP TABLE IF EXISTS `proyectos`;
+CREATE TABLE `proyectos` (
+  `id` int NOT NULL,
+  `fecha_inicio` date NOT NULL,
+  `fecha_fin` date NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `descripcion` mediumtext NOT NULL,
+  `ubicacion` mediumtext NOT NULL,
+  `estado` varchar(50) NOT NULL,
+  `presupuesto` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Volcado de datos para la tabla `proyectos`
+--
+
+INSERT INTO `proyectos` (`id`, `fecha_inicio`, `fecha_fin`, `nombre`, `descripcion`, `ubicacion`, `estado`, `presupuesto`) VALUES
+(1, '2024-10-12', '2024-10-12', 'Instalación de Panel Solar', 'Se necesita la instalación en alimentos maravilla el conjunto de paneles solares.', '6ta ave. A zona 10 ciudad de Guatemala', 'Sin iniciar', '10000'),
+(2, '2024-10-19', '2024-12-24', 'Implementación antenas RF', 'N/A', 'Colonia el Esfuezo, Escuintla', 'En Progreso', '50000');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tareas`
+--
+
+DROP TABLE IF EXISTS `tareas`;
+CREATE TABLE `tareas` (
+  `id` int NOT NULL,
+  `fecha_inicio` date NOT NULL,
+  `fecha_fin` date NOT NULL,
+  `nombre_tarea` varchar(50) NOT NULL,
+  `estado` varchar(50) NOT NULL,
+  `descripcion` mediumtext NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Volcado de datos para la tabla `tareas`
+--
+
+INSERT INTO `tareas` (`id`, `fecha_inicio`, `fecha_fin`, `nombre_tarea`, `estado`, `descripcion`) VALUES
+(2, '2024-10-05', '2024-10-08', 'Compra de insumos', '1', 'mensaje de prueba');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `id` int NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `user_type` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`, `user_type`) VALUES
+(1, 'Juan', '1234', '1'),
+(2, 'bulux', '0192', '2'),
+(3, 'Jairon', '12345', '1');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `user_type`
+--
+
+DROP TABLE IF EXISTS `user_type`;
+CREATE TABLE `user_type` (
+  `id` int NOT NULL,
+  `nombre` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Volcado de datos para la tabla `user_type`
+--
+
+INSERT INTO `user_type` (`id`, `nombre`) VALUES
+(1, 'Admin'),
+(2, 'user');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `empleados`
+--
+ALTER TABLE `empleados`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `materiales`
+--
+ALTER TABLE `materiales`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `proyectos`
+--
+ALTER TABLE `proyectos`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `tareas`
+--
+ALTER TABLE `tareas`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `user_type`
+--
+ALTER TABLE `user_type`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `empleados`
+--
+ALTER TABLE `empleados`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `materiales`
+--
+ALTER TABLE `materiales`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `proyectos`
+--
+ALTER TABLE `proyectos`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `tareas`
+--
+ALTER TABLE `tareas`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `user_type`
+--
+ALTER TABLE `user_type`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
