557c5b2c4fd71e285210d26673782d12db59c2e3
Fernelixia <jdeleong40@miumg.edu.gt>
2024-11-06 15:55:07 -0600

    update

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const express = require('express');
 const axios = require('axios');
 const app = express();
 
-app.get('/proyecto', async (req, res) => {
+app.get('/proyectos', async (req, res) => {
   try {
-    const response = await axios.get('https://desarrollo-18y4.onrender.com/proyecto.php');
+    const response = await axios.get('https://desarrollo-18y4.onrender.com/proyectos.php');
     res.send(response.data);
   } catch (error) {
     res.status(500).send('Error al acceder al archivo PHP');
