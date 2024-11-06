5090e874a9cc67eb1d8b5172b363f63853a6a388
Fernelixia <jdeleong40@miumg.edu.gt>
2024-11-06 16:20:55 -0600

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 
 // Configuración de la conexión a la base de datos
 const db = mysql.createConnection({
-  host: 'localhost',
+  host: '100.20.92.101',
   user: 'root',
   password: '',
   database: 'umg'
